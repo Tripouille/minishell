77a85f972479e49d2ba30f4bd5ca3b8bfe0eafbd
Chinimala <astrid.allemandou@free.fr>
2020-06-25 16:07:44 +0200

    Fix leak for cd home

--- srcs/builtins/builtin_cd.c
+++ srcs/builtins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: aalleman <aalleman@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 04:31:23 by jgambard          #+#    #+#             */
-/*   Updated: 2020/06/25 15:56:02 by aalleman         ###   ########lyon.fr   */
+/*   Updated: 2020/06/25 16:07:33 by aalleman         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,31 @@ void		set_pwd_for_cd(t_lst *args)
 	set_variable(new_variable, 0);
 }
 
+void		cd_home(void)
+{
+	char		*join;
+	char		new_variable[PATH_MAX];
+
+	if (get_variable_pos("HOME") == -1)
+		minishell_error("cd", "« HOME » undefined",
+							ERROR_STATUS);
+	else if (chdir(get_variable_value("HOME")) == -1)
+		usage_error("cd", strerror(errno),
+				get_variable_value("HOME"), ERROR_STATUS);
+	else
+	{
+		g_status = SUCCESS_STATUS;
+		join = ft_strjoin(2, "PWD=", get_variable_value("HOME"));
+		ft_strlcpy(new_variable, join, ft_strlen(join) + 1);
+		free(join);
+		set_variable(new_variable, 0);
+	}
+}
+
 void		builtin_cd(t_lst *args)
 {
 	if (!args->next)
-	{
-		if (get_variable_pos("HOME") == -1)
-			minishell_error("cd", "« HOME » undefined",
-								ERROR_STATUS);
-		else if (chdir(get_variable_value("HOME")) == -1)
-			usage_error("cd", strerror(errno),
-					get_variable_value("HOME"), ERROR_STATUS);
-		else
-		{
-			g_status = SUCCESS_STATUS;
-			set_variable(ft_strjoin(2, "PWD=", get_variable_value("HOME")), 0);
-		}
-	}
+		cd_home();
 	else if (args->next && args->next->next)
 		usage_error("cd", TOO_MANY_ARGUMENTS, "", ERROR_STATUS);
 	else if (chdir(get_arg_value(args, 1)) == -1)
